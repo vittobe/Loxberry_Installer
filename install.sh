f5e70dc54108790c9e0a526f7e05afead390ff66
Michael Schlenstedt <Michael@schlenn.net>
2023-11-22 09:33:36 +0100

    Changes for seperating Deb12 and Deb11

--- install.sh
+++ install.sh
@@ -334,7 +334,7 @@ TITLE "Installing additional software packages from apt repository..."
 /boot/dietpi/func/dietpi-set_software apt cache clean
 apt update
 
-if [ -e $LBHOME/packages.txt ]; then
+if [ -e "$LBHOME/packages${TARGET_VERSION_ID}.txt" ]; then
         PACKAGES=""
         echo ""
         while read entry
@@ -357,9 +357,9 @@ if [ -e $LBHOME/packages.txt ]; then
                         OK "Add package $PACKAGE to the installation queue..."
                         PACKAGES+="$PACKAGE "
                 fi
-        done < $LBHOME/packages.txt
+        done < "$LBHOME/packages${TARGET_VERSION_ID}.txt"
 else
-        FAIL "Could not find packages list: $LBHOME/packages.txt.\n"
+        FAIL "Could not find packages list: $LBHOME/packages${TARGET_VERSION_ID}.txt.\n"
         exit 1
 fi
 
